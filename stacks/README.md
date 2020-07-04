45560eb5c49ad5eea3708b46afab1959cf352319
Pranay0302 <pka.pranayandra02@gmail.com>
2020-07-04 17:12:03 +0530

    initial commit

---
+++ stacks/README.md
@@ -0,0 +1,27 @@
+# Stacks 
+
+## Main Stack Operations
+- Push (int data)
+- int Pop()
+
+## Auxiliary Stack Operations
+- int Top()
+- int Size()
+- int isEmptyStack()
+- int isFullStack()
+
+
+
+```cpp
+Push(data)        : pushes the data onto the stack
+
+Pop()             : removes the last inserted element
+
+Top()             : returning the last inserted element
+
+isEmptyStack()    : checking if any elements are present in the stack  
+
+isFullStack()     : checking whether the stack is full or not 
+
+```
+
---
+++ stacks/basicStacks.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+#define max 1000
+
+using namespace std;
+
+static int top = -1;
+int *stack = new int[max];
+
+void push(int data){
+    if(top==max){
+        cout << "Stack Overflow" << "\n";
+    }
+
+    top++;
+    stack[top] = data;
+}
+
+void pop(){
+    if(top == -1){
+        cout << "Stack Underflow" << "\n";
+    }else{
+        cout << "The popped element is "<< stack[top] << "\n";
+        top--;
+    }
+}
+
+bool isFullStack(){
+    return ( top == max ) ? true:false;
+}
+
+bool isEmptyStack(){
+    return ( top== -1 ) ? true:false;
+}
+
+int main()
+{
+    int yup,val;
+    cout << "1. push " << "\n"; 
+    cout << "2. pop " << "\n"; 
+    cout << "3. isFullStack " << "\n"; 
+    cout << "4. isEmptyStack " << "\n"; 
+    do {
+        cout << "Enter Anyone: " << "\n";
+        cin >> val;
+        switch(yup) {
+            case 1: 
+            cout << "Enter the val." << "\n";
+            cin >> val;
+            push(val);
+            break;
+
+            case 2:
+            pop();
+            break;
+
+            case 3:
+            isFullStack();
+            break;
+
+            case 4:
+            isEmptyStack();
+            break;
+
+            default: 
+            cout << "Nope";
+        }
+
+    }while(yup != 4);
+    
+    return 0;
+}
